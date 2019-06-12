4af34d9829b0b65bcd409a7aafd0b25d65ff56a8
Inna Timonova <innatimonova200@gmail.com>
2019-06-12 14:56:07 +0300

    1.3. JAvaDoc. Code convention [#147522]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate
+*
+*@author inna.timonova (ms.timonovai@mail.ru)
+*/
+
 public class Calculate {
-	
+	/**
+	*Main
+	*@params args - args.
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
