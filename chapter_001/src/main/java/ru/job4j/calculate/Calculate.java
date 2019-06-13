3e464aaa828551dc93a6659834c6b14a0a1cde0b
Inna Timonova <innatimonova200@gmail.com>
2019-06-13 16:52:42 +0300

    1.5. Checkstyle[#147524]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -11,7 +11,7 @@ public class Calculate {
 	*Main
 	*@params args - args.
 	*/
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 	
