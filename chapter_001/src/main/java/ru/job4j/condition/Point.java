4f6df24324bb25231249918eac866c74d5f78609
Inna Timonova <innatimonova200@gmail.com>
2019-06-20 19:26:43 +0300

    fixed javadoc in Point

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -16,13 +16,9 @@ public class Point {
      *@return the distance between 2 points in the coordinates system.
      */
     public double distance(int x1, int y1, int x2, int y2) {
-        /*The first value raised to a 2nd power*/
         double first = Math.pow(x2 - x1, 2);
-        /*The first value raised to a 2nd power*/
         double second = Math.pow(y2 - y1, 2);
-        /*The sum of 2 values*/
         double third = first + second;
-        /*The square root of value third*/
         return Math.sqrt(third);
     }
 }
