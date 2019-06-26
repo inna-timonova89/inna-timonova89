03641171d6cd7738cb8e999ef9e2002495eb4a37
Inna Timonova <innatimonova200@gmail.com>
2019-06-26 17:11:50 +0300

    fixed MultiMaxTest

--- chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -16,4 +16,22 @@ public class MultiMaxTest {
         int result = check.max(1, 4, 2);
         assertThat(result, is(4));
     }
+    @Test
+    public void whenThirdMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(2, 1, 4);
+        assertThat(result, is(4));
+    }
+    @Test
+    public void whenFirstMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(4, 2, 1);
+        assertThat(result, is(4));
+    }
+    @Test
+    public void whenEqualMax() {
+        MultiMax check = new MultiMax();
+        int result = check.max(4, 4, 4);
+        assertThat(result, is(4));
+    }
 }
