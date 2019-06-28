90a264ea2da3fafcaea61b4da2fc04ea5c7390b2
Inna Timonova <innatimonova200@gmail.com>
2019-06-28 16:35:21 +0300

    added Board, BoardTest

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+/**
+ * Возвращает шахматную доску из символов x и пробелов.
+ * @return Результат сложения строк из сиволово и пробелов и перенос этих строк.
+ * @author inna.timonova (ms.timonovai@mail.ru)
+ */
+public class Board {
+        public String paint(int width, int height) {
+            StringBuilder screen = new StringBuilder();
+            String ln = System.lineSeparator();
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    // условие проверки, что писать пробел или X
+                    // Выше в задании мы определили закономерность, когда нужно проставлять X
+                    if ((i + j) % 2 == 0) {
+                        screen.append("X");
+                    } else {
+                        screen.append(" ");
+                    }
+                }
+                // добавляем перевод на новую строку.
+                screen.append(ln);
+            }
+            return screen.toString();
+        }
+    }
