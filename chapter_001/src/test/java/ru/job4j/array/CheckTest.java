ce78df61777dc4b2f5715f1ff0c005d749d5d36a
Inna Timonova <innatimonova200@gmail.com>
2019-07-05 18:08:30 +0300

    added Check, CheckTest

---
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author inna.timonova (ms.timonovai@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class CheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+    @Test
+    public void whenDataMonoByFalseThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {false, false, false};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+    @Test
+    public void whenDataMonoByTrueThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true, false};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+    @Test
+    public void whenDataNotMonoByFalseThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, false, true, false};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+}
