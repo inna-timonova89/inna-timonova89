345be1a1f13a9b800e41da0238f4e557e81f5292
Inna Timonova <innatimonova200@gmail.com>
2019-07-08 16:32:50 +0300

    changed BubbleSort

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -7,13 +7,10 @@ package ru.job4j.array;
  */
 public class BubbleSort {
     public int[] sort(int[] array) {
-        boolean isSorted = false;
         int temp;
-        while (!isSorted) {
-            isSorted = true;
-            for (int i = 0; i < array.length - 1; i++) {
+        for (int j = 0; j < array.length - 1; j++) {
+            for (int i = 0; i < array.length - 1 - j; i++) {
                 if (array[i] > array[i + 1]) {
-                    isSorted = false;
 
                     temp = array[i];
                     array[i] = array[i + 1];
