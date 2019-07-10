bd1c167e655570b9e5c2569a8f950d5e7c0b48c9
Inna Timonova <innatimonova200@gmail.com>
2019-07-10 13:14:09 +0300

    fixed MatrixCheck

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -9,7 +9,7 @@ public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         int n = data.length - 1;
         for (int i = 0; i < n; i++) {
-            if (!((data[i][i] == data[i + 1][i + 1]) && data[i][n - i] == data[i + 1][n - 1 - i])) {
+            if (((data[i][i] != data[i + 1][i + 1]) || data[i][n - i] != data[i + 1][n - 1 - i])) {
             return false;
         }
     }
