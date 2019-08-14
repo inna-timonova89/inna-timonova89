f7aeda55adb5469ee70a31ddf00e235ade05cdae
Inna Timonova <innatimonova200@gmail.com>
2019-08-14 14:39:55 +0300

    Расстояние между точками в трехмерном пространстве.[#147596]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -16,6 +16,11 @@ public class Point {
     private int y;
 
     /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int z;
+
+    /**
      * Конструктор, который принимает начальное состояние объекта "точка"
      * @param first координата x
      * @param second координата y
@@ -25,12 +30,25 @@ public class Point {
         this.y = second;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
 
     public double distance(Point pnt) {
         return sqrt(pow(this.x - pnt.x, 2) + pow(this.y - pnt.y, 2));
     }
 
+    public double distance3d(Point pnt) {
+        return sqrt(pow(this.x - pnt.x, 2) + pow(this.y - pnt.y, 2) + pow(this.z - pnt.z, 2));
+    }
+
     public void info() {
         System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+    public void info2() {
+        System.out.println(String.format("Point[%s, %s, %s]", this.x, this.y, this.z));
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -9,11 +9,18 @@ public class PointTest {
     public void whenZeroAndTenThenTen() {
         Point first = new Point(0, 0);
         Point second = new Point(0, 10);
+        Point third = new Point(0, 0, 0);
+        Point fourth = new Point(0, 0, 10);
         double result = first.distance(second);
+        double result2 = third.distance3d(fourth);
         first.info();
         second.info();
+        third.info();
+        fourth.info();
         System.out.println(String.format("Result is %s", result));
+        System.out.println(String.format("Result is %s", result2));
         assertThat(result, is(10D));
+        assertThat(result2, is(10D));
     }
 
     @Test
@@ -21,6 +28,9 @@ public class PointTest {
         Point point = new Point(0, 0);
         double result = point.distance(point);
         assertThat(result, is(0D));
+        Point point2 = new Point(0, 0, 10);
+        double result2 = point2.distance3d(point);
+        assertThat(result2, is(10D));
     }
 
     @Test
@@ -29,5 +39,9 @@ public class PointTest {
         first.info();
         Point second = new Point(2, 2);
         second.info();
+        Point third = new Point(1, 3, 5);
+        third.info2();
+        Point fourth = new Point(2, 4, 6);
+        fourth.info2();
     }
 }
