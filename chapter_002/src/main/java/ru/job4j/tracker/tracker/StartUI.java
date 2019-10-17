105207f828edecb5e64e211e5007637bb432dee0
Inna Timonova <innatimonova200@gmail.com>
2019-10-17 15:15:57 +0300

    1. Обеспечить бесперебойную работу приложения Tracker. [#789]

--- chapter_002/src/main/java/ru/job4j/tracker/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/tracker/StartUI.java
@@ -73,7 +73,7 @@ public class StartUI {
         boolean run = true;
         while (run) {
             this.showMenu(actions);
-            int select = input.askInt("Select: ");
+            int select = input.askInt("Select: ", actions.length);
             UserAction action = actions[select];
             run = action.execute(input, tracker);
         }
@@ -211,11 +211,11 @@ public class StartUI {
      * @param args
      */
     public static void main(String[] args) {
-        Input input = new ConsoleInput();
+        Input validate = new ValidateInput();
         Tracker tracker = new Tracker();
-        UserAction[] actions = new UserAction[]{
+        UserAction[] actions = {
                 new CreateAction()
         };
-        new StartUI().init(input, tracker, actions);
+        new StartUI().init(validate, tracker, actions);
     }
 }
