60ece76b154fd9ef3740c7058b66908c052aa648
Inna Timonova <innatimonova200@gmail.com>
2019-10-29 15:22:15 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.[#147585]

--- chapter_002/src/main/java/ru/job4j/tracker/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/tracker/StartUI.java
@@ -211,7 +211,8 @@ public class StartUI {
      * @param args
      */
     public static void main(String[] args) {
-        Input validate = new ValidateInput();
+        Input input = new ConsoleInput();
+        Input validate = new ValidateInput(input);
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction()
---
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import ru.job4j.tracker.tracker.StubInput;
+import ru.job4j.tracker.tracker.ValidateInput;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        ByteArrayOutputStream mem = new ByteArrayOutputStream();
+        PrintStream out = System.out;
+        System.setOut(new PrintStream(mem));
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[] {"invalid", "1"})
+        );
+        input.askInt("Enter", 1);
+        assertThat(
+                mem.toString(),
+                is(String.format("Please enter the valid data again " + System.lineSeparator()))
+        );
+        System.setOut(out);
+    }
+}
