ef4c2a899508d52adf04d9aa757e17b7a058e2ee
Inna Timonova <innatimonova200@gmail.com>
2020-01-25 20:06:39 +0300

    Сдача в кофе машине. [#34741]

---
+++ chapter_001/src/main/java/ru/job4j/sort/Machine.java
@@ -0,0 +1,21 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] COINS = {10, 5, 2, 1};
+
+    public int[] change(int value, int price) {
+        value -= price;
+        int[] rsl = new int[value / 10 + 1];
+        int[] coins = new int[]{10, 5, 2, 1};
+        int iter = 0;
+        for (int coin : coins) {
+            while (value - coin >= 0) {
+                rsl[iter++] = coin;
+                value -= coin;
+            }
+        }
+        return Arrays.copyOf(rsl, iter);
+    }
+}
