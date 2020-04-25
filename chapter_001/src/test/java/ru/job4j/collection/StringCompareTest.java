f3cb64c126ebb5a185908bf9b2d950f0b0d80d91
Inna Timonova <innatimonova200@gmail.com>
2020-04-25 17:49:21 +0300

    3. Компаратор для строк.[#147537]

---
+++ chapter_001/src/test/java/ru/job4j/collection/StringCompareTest.java
@@ -0,0 +1,70 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class StringCompareTest {
+    @Test
+    public void whenStringsAreEqualThenZero() {
+        StringCompare compare = new StringCompare();
+        int result = compare.compare(
+                "Ivanov",
+                "Ivanov"
+        );
+        assertThat(result, is(0));
+    }
+
+    @Test
+    public void whenLeftLessThanRightResultShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int result = compare.compare(
+                "Ivanov",
+                "Ivanova"
+        );
+        assertThat(result, lessThan(0));
+    }
+
+    @Test
+    public void whenLeftGreaterThanRightResultShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int result = compare.compare(
+                "Petrov",
+                "Ivanova"
+        );
+        assertThat(result, greaterThan(0));
+    }
+
+    @Test
+    public void secondCharOfLeftGreaterThanRightShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int result = compare.compare(
+                "Petrov",
+                "Patrov"
+        );
+        assertThat(result, greaterThan(0));
+    }
+
+    @Test
+    public void secondCharOfLeftLessThanRightShouldBeNegative() {
+        StringCompare compare = new StringCompare();
+        int result = compare.compare(
+                "Patrova",
+                "Petrov"
+        );
+        assertThat(result, lessThan(0));
+    }
+
+    @Test
+    public void secondCharOfLeftLessThanRightButThirdCharOfRightGreaterThanRightShouldBePositive() {
+        StringCompare compare = new StringCompare();
+        int rst = compare.compare(
+                "Petrov",
+                "Pavlov"
+        );
+        assertThat(rst, greaterThan(0));
+    }
+}
